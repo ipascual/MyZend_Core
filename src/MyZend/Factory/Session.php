66a88a4c16871c4217ea8b1ae6b2628830955b76
Ignacio Pascual <ipascual@unexpectedit.com>
2013-08-01 18:44:48 -0500

    Search issue fixed

--- src/MyZend/Factory/Session.php
+++ src/MyZend/Factory/Session.php
@@ -2,7 +2,6 @@
 namespace MyZend\Factory;
 
 use Zend\Session\Container;
-use MyZend\Factory\Session\Search;
 
 class Session
 {
@@ -10,9 +9,6 @@ class Session
 	
 	public function __construct() {
 		$this->session = new Container('base');
-		if(! $this->session->offsetExists("search")) {
-			$this->set("search", new Search());
-		}
 	}
 
 	public function set($key, $value) {
